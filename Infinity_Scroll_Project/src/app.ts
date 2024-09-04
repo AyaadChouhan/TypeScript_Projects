e73c1bb091fb8ac7c4bd23465c62d3ed2615be10
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-09-04 13:27:15 +0530

    Update app.ts

--- Infinity_Scroll_Project/src/app.ts
+++ Infinity_Scroll_Project/src/app.ts
@@ -40,13 +40,10 @@ function singleImgFunc(e: Event): void {
   let target = e.target as HTMLElement;
   let el = target.cloneNode(true) as HTMLElement;
   if (target.tagName === "IMG" && singleImgCont.children.length === 0) {
-    console.log('clicked')
     singleImgCont.append(el);
   }
 
   if (target.tagName === "IMG" && singleImgCont.children.length >= 1) {
-    console.log(singleImgCont.children.length)
-    console.log('hello')
     singleImgCont.innerHTML = "";
     if (singleImgCont.children.length <= 0) {
         singleImgCont.append(leftI);
